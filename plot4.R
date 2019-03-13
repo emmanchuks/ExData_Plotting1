a49735d0e7e0b665ac542b94da75ced1821d65b1
Ezeonyebuchi Emmanuel Chukwuma <26045664+emmanchuks@users.noreply.github.com>
2019-03-13 22:58:19 +0100

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-#dataFile <- "./data/household_power_consumption.txt"
+df0 <- "./data/household_power_consumption.txt"
 df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetdf <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
 
