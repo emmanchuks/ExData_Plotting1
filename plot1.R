bdb8e1c89158831bc6ad868f23ed889cfe271974
Ezeonyebuchi Emmanuel Chukwuma <26045664+emmanchuks@users.noreply.github.com>
2019-03-13 22:32:52 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,14 @@
+#df <- "./data/household_power_consumption.txt"
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetdf <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetdf)
+str(subSetdf)
+complete.cases(subSetdf)
+subSetdf1 <- subSetdf[complete.cases(subSetdf), ]
+#subsetdf1 <- ?.omit(subSetdf)
+
+globalActivePower <- as.numeric(subSetdf1$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
