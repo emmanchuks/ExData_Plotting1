07ae747f1e69d4cf8521e234a2b05db3e94a59e9
Ezeonyebuchi Emmanuel Chukwuma <26045664+emmanchuks@users.noreply.github.com>
2019-03-13 22:43:18 +0100

    Add files via upload

---
+++ plot5.R
@@ -0,0 +1,38 @@
+#dataFile <- "./data/household_power_consumption.txt"
+df <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetdf <- df[df$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetdf)
+str(subSetdf)
+complete.cases(subSetdf)
+subSetdf1 <- subSetdf[complete.cases(subSetdf), ]
+#subsetdf1 <- ?.omit(subSetdf)
+
+datetime <- strptime(paste(subSetdf1$Date, subSetdf1$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subSetdf1$Global_active_power)
+energySubMetering1 <- as.numeric(subSetdf1$Sub_metering_1)
+energySubMetering2 <- as.numeric(subSetdf1$Sub_metering_2)
+energySubMetering3 <- as.numeric(subSetdf1$Sub_metering_3)
+globaReactivePower <- as.numeric(subSetdf1$Global_reactive_power)
+
+par(mfrow = c(1, 2), mar = c(4, 4, 2, 0), oma = c(0, 0, 2, 0), pty="s")
+png("plot5.png", width=480, height=480)
+datetime <- strptime(paste(subSetdf1$Date, subSetdf1$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subSetdf1$Global_active_power)
+energySubMetering1 <- as.numeric(subSetdf1$Sub_metering_1)
+energySubMetering2 <- as.numeric(subSetdf1$Sub_metering_2)
+energySubMetering3 <- as.numeric(subSetdf1$Sub_metering_3)
+globaReactivePower <- as.numeric(subSetdf1$Global_reactive_power)
+
+png("plot5.png", width=480, height=480)
+
+par(mfrow = c(1, 2), mar = c(4, 4, 2, 0), oma = c(0, 0, 2, 0), pty="s")
+plot(datetime, energySubMetering1, type="l", xlab="", ylab="energy SubMetering")
+lines(datetime, energySubMetering2, type="l", col="red")
+lines(datetime, energySubMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd = 3, col= c("black", "red", "blue"))
+
+plot(datetime, globaReactivePower, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
+
+
